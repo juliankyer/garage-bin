5ae67e2a274fbd53d6bcf3e9176b3547f45ccc98
Julian Kyer <julian.kyer@gmail.com>
2017-06-01 12:23:58 -0600

    Add test sketches

---
+++ test/frontend.spec.js
@@ -0,0 +1,23 @@
+describe('front-end usage', () => {
+  it('should allow you to put something into the garage', () => {
+    
+  });
+  
+  it('should not allow you to add something if data is missing', () => {
+    
+  });
+  
+  it('should start with the garage closed', () => {
+    
+  });
+  
+  it('should display garage items when the door is open', () => {
+    
+  });
+  
+  it('should allow you to remove an item', () => {
+    
+  });
+  
+  
+});
