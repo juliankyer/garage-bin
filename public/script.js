4265526e3fc622a672cae38308bf0cd108ec17eb
Julian Kyer <julian.kyer@gmail.com>
2017-06-01 17:51:20 -0600

    Add tally function

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-
-
 const fetchItems = () => {
   fetch('/api/v1/items')
     .then(response => response.json())
@@ -25,15 +23,29 @@ const appendItems = (items) => {
     const cleanliness = item.cleanliness;
     
     $('ul').append(`
-      <li class="item-card" id=${id}>
-        <h3>${name}</h3>
-        <p>Reason for being here: ${reason}</p>
-        <div class="status-wrapper">
-          <p>Cleanliness: ${cleanliness}</p>
+      <li class="item-card ${cleanliness}" id=${id}>
+        <h3 class="show-details">${name}</h3>
+        <div class="details">
+          <p>Excuse: ${reason}</p>
+          <div class="status-wrapper">
+            <p>Cleanliness: ${cleanliness}</p>
+          </div>
         </div>
       </li>
     `)
   });
+}
+
+const tallyItems = () => {
+  let $items = $('.item-card').length;
+  let $fancy = $('.sparkling').length;
+  let $dusty = $('.dusty').length;
+  let $rancid = $('.rancid').length;
+  
+  $('#fancy-tally').text(`Fancy things: ${$fancy}`);
+  $('#dusty-tally').text(`Dusty things: ${$dusty}`);
+  $('#rancid-tally').text(`Gross things: ${$rancid}`);
+  $('#total').text(`Total things: ${$items}`);
 }
 
 $('#add-btn').on('click', function(e) {
@@ -49,6 +61,20 @@ $('#add-btn').on('click', function(e) {
   
   $('#item-name').val('');
   $('#item-reason').val('');
+});
+
+$('#opener').on('click', function() {
+  $('ul').toggleClass('door-closed');
+  $('.contents').toggleClass('door-closed');
+  $('#opener').toggleClass('open');
+  
+  if ($('#opener').hasClass('open')) {
+    $('#opener').text('Hide This Mess');
+  } else {
+    $('#opener').text('Open Garage');
+  }
+  
+  tallyItems();
 });
 
 $(document).ready(() => {
